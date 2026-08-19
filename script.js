1fc950cfbf0734e61bca20cf61660687536d667b
Nishmitha <nishmithar277@gmail.com>
2026-08-19 21:05:30 +0530

    Updated portfolio with dark mode fixes

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-const button = document.getElementById("themeButton");
+const button = document.getElementById("modeBtn");
 
-button.addEventListener("click", function() {
-    document.body.classList.toggle("dark");
-=======
-const button = document.getElementById("themeButton");
-
-button.addEventListener("click", function() {
-    document.body.classList.toggle("dark");
->>>>>>> 424c880a4c5c3e803c7c4f97684486216815c587
+button.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
 });
